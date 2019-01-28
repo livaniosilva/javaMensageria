e624d703ab5d9b98dfb2cb4d82c1e54fff907c0f
lfernando <livanio_silva@ecad.org.br>
2019-01-28 13:29:23 -0200

    java mensageria

--- src/com/github/livaniosilva/java/rabbitMQ/Receiver.java
+++ src/com/github/livaniosilva/java/rabbitMQ/Receiver.java
@@ -24,6 +24,7 @@ public class Receiver {
 	            System.out.println(" [x] Received '" + message + "'");
 	    };
 	    channel.basicConsume(QUEUE_NAME, true, deliverCallBack, consumerTag -> { });
+	    
 	  }
 	
 
